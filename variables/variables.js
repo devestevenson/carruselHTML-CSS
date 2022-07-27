b52300a8ccbb903653379bf76ae5f63fb36d14b1
dev estevenson <107928127+devestevenson@users.noreply.github.com>
2022-07-26 22:45:28 -0500

    Practica de como hacer un carrusel de noticias con HTML&CSS

---
+++ variables/variables.js
@@ -0,0 +1,70 @@
+// //creando una VARIABLE con "var" y "let"
+// var nombreMasculino = "Juan";
+// console.log(nombreMasculino)
+
+// let nombreFemenino = "Juana";
+// console.log(nombreFemenino)
+
+// //creando una VARIABLE numerícas concatenar
+// var x = 6 + 7;
+// console.log(x);
+
+// let y = 67 + 8
+// console.log(y);
+
+// //creando una VARIABLE con letras y números concatenar
+// var q = 'Juan, ' + 51;
+// console.log(q);
+
+// let w = 'Juana, '+ 60;
+// console.log(w);
+
+// //concatenar variables
+// var parejaHumana = "Juana"+" y "+"Juan";
+// console.log(parejaHumana);
+
+// //Otras variables a modo de practica con tildes invertidas `` y concatenacion
+// var nombre = "Jhon";
+// var edad = 25;
+// var datos = `mi nombre es: ${nombre} y tengo ${edad} años.`;
+// console.log(datos);
+
+// let humanos = 'humanos';
+// let animales = 'animales';
+// let plantas = 'plantas';
+// let especies = `En el planeta tierra habitan ${humanos}, ${animales}, ${plantas}, etc.`;
+// console.log(especies);
+
+// //uso de constante, se usa para tener valores fijos, ejemplo "PI = 3.14"
+// const PI = 3.14;
+// console.log(PI); //una cosnt solo pertenece a un unico valor.
+
+
+let kilometros=prompt("ingrese una cantidad de kilometros");
+millas = 0,621371
+resultado = kilometros * millas
+
+alert(`El equivalente de ${kilometros} a millas es: ${resultado}`);
+
+
+
+
+
+
+
+
+
+
+
+
+//Centrar en CSS.
+
+//Forma clasica CSS
+position: ab
+
+
+
+
+
+
+
